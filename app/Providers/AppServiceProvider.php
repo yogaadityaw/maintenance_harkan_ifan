8b30da93605919075d9fe3a3d12be1026e0fd655
yogaadityaw <yogaaawi12345@gmail.com>
2024-08-17 07:23:09 +0700

    change ngrok permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env(key: 'APP_ENV') !=='local') {
+            URL::forceScheme(scheme:'https');
+          }
     }
 
     /**
